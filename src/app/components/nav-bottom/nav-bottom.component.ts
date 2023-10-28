4c66dbc61b1bfe4376818e901b9995ac60335af0
kkout <koutsompinaskk@gmail.com>
2023-10-28 17:24:59 +0300

    first commit

---
+++ src/app/components/nav-bottom/nav-bottom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bottom',
+  templateUrl: './nav-bottom.component.html',
+  styleUrls: ['./nav-bottom.component.css']
+})
+export class NavBottomComponent {
+
+}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {} from 'ngx-bootstrap'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+}
